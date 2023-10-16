d71a80c134a39d7a113dab835aa34d6e18c5e125
eldivategar <tegareldiva@students.amikom.ac.id>
2023-10-16 10:11:08 +0700

    prod

--- skillupkids/settings/prod.py
+++ skillupkids/settings/prod.py
@@ -3,5 +3,5 @@ from .settings import *
 
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 DEBUG = False
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '193.203.161.35']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
