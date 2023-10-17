b08757b4738118f631d0b44dfd204c879b9e5ab8
eldivategar <tegareldiva@students.amikom.ac.id>
2023-10-17 21:37:05 +0700

    update SK

--- skillupkids/settings/prod.py
+++ skillupkids/settings/prod.py
@@ -1,7 +1,7 @@
 import os
 from .settings import *
 
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = 'django-insecure-shst$+ksll6x&wmyrsy69%7r%(sd(o=9g2lw61d_16xkdv*n-)'
 DEBUG = False
 ALLOWED_HOSTS = ['*']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
