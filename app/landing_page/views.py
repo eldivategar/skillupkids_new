3a58bd8b50ed9cb77bf84dd8ecfca58e5000e117
eldivategar <tegareldiva@students.amikom.ac.id>
2023-11-04 22:41:57 +0700

    update logic

--- app/landing_page/views.py
+++ app/landing_page/views.py
@@ -4,43 +4,43 @@ from app.helpers.utils import get_member_data, get_mitra_data
 def home(request):
     if 'customer_id' not in request.session:
         return render(request, 'landing_page/index.html')
-
-    if request.session['user_type'] == 'mitra':
-        data = get_mitra_data(request)
-        return render(request, 'landing_page/index.html', {'data': data})
     else:
-        data = get_member_data(request)
-        return render(request, 'landing_page/index.html', {'data': data})
+        if request.session['user_type'] == 'mitra':
+            data = get_mitra_data(request)
+            return render(request, 'landing_page/index.html', {'data': data})
+        else:
+            data = get_member_data(request)
+            return render(request, 'landing_page/index.html', {'data': data})
 
 def about(request):
     if 'customer_id' not in request.session:
         return render(request, 'landing_page/about.html')
-    
-    if request.session['user_type'] == 'mitra':
-        data = get_mitra_data(request)
-        return render(request, 'landing_page/about.html', {'data': data})
     else:
-        data = get_member_data(request)
-        return render(request, 'landing_page/about.html', {'data': data})
+        if request.session['user_type'] == 'mitra':
+            data = get_mitra_data(request)
+            return render(request, 'landing_page/about.html', {'data': data})
+        else:
+            data = get_member_data(request)
+            return render(request, 'landing_page/about.html', {'data': data})
 
 def contact(request):
     if 'customer_id' not in request.session:
         return render(request, 'landing_page/contact.html')
-    
-    if request.session['user_type'] == 'mitra':
-        data = get_mitra_data(request)
-        return render(request, 'landing_page/contact.html', {'data': data})
     else:
-        data = get_member_data(request)
-        return render(request, 'landing_page/contact.html', {'data': data})
+        if request.session['user_type'] == 'mitra':
+            data = get_mitra_data(request)
+            return render(request, 'landing_page/contact.html', {'data': data})
+        else:
+            data = get_member_data(request)
+            return render(request, 'landing_page/contact.html', {'data': data})
 
 def class_list(request):
     if 'customer_id' not in request.session:
         return render(request, 'landing_page/class-list.html')
-    
-    if request.session['user_type'] == 'mitra':
-        data = get_mitra_data(request)
-        return render(request, 'landing_page/class-list.html', {'data': data})
     else:
-        data = get_member_data(request)
-        return render(request, 'landing_page/class-list.html', {'data': data})
+        if request.session['user_type'] == 'mitra':
+            data = get_mitra_data(request)
+            return render(request, 'landing_page/class-list.html', {'data': data})
+        else:
+            data = get_member_data(request)
+            return render(request, 'landing_page/class-list.html', {'data': data})
