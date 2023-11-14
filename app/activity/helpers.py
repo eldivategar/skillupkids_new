d09c37e9dcacc9488899af24f7e1e3c4e425e2dd
eldivategar <tegareldiva@students.amikom.ac.id>
2023-11-14 23:09:30 +0700

    fix bug testimoni

--- app/activity/helpers.py
+++ app/activity/helpers.py
@@ -129,7 +129,7 @@ def get_testimonial(num=5):
     all_data = []
 
     for testimonial in get_all_testimonial:
-        member_data = Member.objects.filter(name=testimonial.member.name)
+        member_data = Member.objects.get(name=testimonial.member.name)
         # mitra_data = Mitra.objects.get(name=testimonial.mitra.name)
         activity_data = ActivityList.objects.filter(activity_name=testimonial.activity.activity_name)
         testimonial_data = testimonial.testimonial_json()           
