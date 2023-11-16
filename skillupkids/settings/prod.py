5d1275400875a1c9a016ee0757b16a9e582085e9
eldivategar <tegareldiva@students.amikom.ac.id>
2023-11-16 10:06:33 +0700

    change db sqlite to mysql

--- skillupkids/settings/prod.py
+++ skillupkids/settings/prod.py
@@ -6,3 +6,12 @@ DEBUG = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
+    }
+}
