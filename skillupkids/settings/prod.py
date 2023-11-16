7190422a68c678cf18931af19eaf192b1c9145bd
eldivategar <tegareldiva@students.amikom.ac.id>
2023-11-16 11:25:25 +0700

    update database to mysql

--- skillupkids/settings/prod.py
+++ skillupkids/settings/prod.py
@@ -9,17 +9,9 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
     }
 }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'OPTIONS': {
-#             'read_default_file': '/etc/mysql/my.cnf',
-#         },
-#     }
-# }
