780f5ef2e0187f52f87e97687c8fbee9b7aacbd5
eldivategar <tegareldiva@students.amikom.ac.id>
2023-11-16 11:29:20 +0700

    fix bug db

--- skillupkids/settings/prod.py
+++ skillupkids/settings/prod.py
@@ -9,9 +9,17 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '/etc/mysql/my.cnf',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {
+#             'read_default_file': '/etc/mysql/my.cnf',
+#         },
+#     }
+# }
