33ba5c67de312839704a24367963739440f60fbc
eldivategar <tegareldiva@students.amikom.ac.id>
2023-12-05 14:34:58 +0700

    build_files

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,2 @@
-python3.9 -m venv env
-source env/bin/activate
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --settings=skillupkids.settings.prod
+python manage.py collectstatic --settings=skillupkids.settings.prod
