4c386a5c819058a244758938c3774a93697ad6f6
eldivategar <tegareldiva@students.amikom.ac.id>
2023-12-05 14:14:10 +0700

    add postgresql railway

--- skillupkids/settings/prod.py
+++ skillupkids/settings/prod.py
@@ -3,15 +3,18 @@ from .settings import *
 
 SECRET_KEY = 'django-insecure-shst$+ksll6x&wmyrsy69%7r%(sd(o=9g2lw61d_16xkdv*n-)'
 DEBUG = False
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '/etc/mysql/my.cnf',
-        },
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': 'postgresql://postgres:B21AfcFDBaAaFFGAgaeF53Aa61-egGf3@roundhouse.proxy.rlwy.net:38061/railway',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'B21AfcFDBaAaFFGAgaeF53Aa61-egGf3',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': 38061,
     }
 }
