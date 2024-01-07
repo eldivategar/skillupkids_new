87f75f10d1d9b3acc4c5d559e9a3253db701bef4
eldivategar <tegareldiva@students.amikom.ac.id>
2024-01-07 17:44:40 +0700

    .

--- skillupkids/management/commands/customsuperuser.py
+++ skillupkids/management/commands/customsuperuser.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
 
-superuser = User.objects.create_superuser(
-    username='admin',
-    email='skillupkidscontact@gmail.com',
-    password='skillupkidsid'
-)
-superuser.save()
+class Command(BaseCommand):
+    help = 'Creates superuser'
+
+    def handle(self, *args, **options):
+        superuser = User.objects.create_superuser(
+            username='admin',
+            email='skillupkidscontact@gmail.com',
+            password='skillupkidsid'
+        )
+        superuser.save()
+        self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
