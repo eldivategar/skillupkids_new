161a5d6ace869b8c50c02bccf7d4056bf38a5b3f
eldivategar <tegareldiva@students.amikom.ac.id>
2024-01-07 16:55:09 +0700

    .

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,11 @@
-pip install virtualenv
-virtualenv env
-source env/bin/activate
+echo "BUILD START"
+
+python3.9 -m venv venv
+    source venv/bin/activate
 
 pip install -r requirements.txt
+
+echo "BUILD END"
+
+echo "Collecting static files..."
 python manage.py collectstatic --settings=skillupkids.settings.prod
