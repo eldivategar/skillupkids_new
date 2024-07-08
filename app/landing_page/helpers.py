bc298595f31e037334dcf878a05ecef66b94e34e
eldivategar <tegareldiva@students.amikom.ac.id>
2024-07-09 00:51:01 +0700

    optimize achievements

--- app/landing_page/helpers.py
+++ app/landing_page/helpers.py
@@ -1,14 +1,13 @@
 from app.models import Member, Mitra, ActivityList
-from asgiref.sync import sync_to_async
 
-async def get_achievements(request):
+def get_achievements(request):
     try:
         # count total member
-        total_member = await sync_to_async(Member.objects.count)()
+        total_member = Member.objects.all().count()
         # count total mitra
-        total_mitra = await sync_to_async(Mitra.objects.count)()
+        total_mitra = Mitra.objects.all().count()
         # count total activity
-        total_activity = await sync_to_async(ActivityList.objects.count)()
+        total_activity = ActivityList.objects.all().count()
         context = {
             'total_member': total_member,
             'total_mitra': total_mitra,
--- app/landing_page/views.py
+++ app/landing_page/views.py
@@ -2,27 +2,26 @@ from django.shortcuts import render, redirect
 from app.helpers.utils import get_member_data, get_mitra_data
 from app.activity.helpers import get_category, get_new_activity, get_testimonial
 from django.views.decorators.cache import cache_page
-from app.models import Member, Mitra, ActivityList, Blog
-from asgiref.sync import sync_to_async
+from app.models import Blog
 from .helpers import get_achievements
 
 # @cache_page(604800)
-async def home(request):
-    category = await sync_to_async(get_category)()
-    new_activity = await sync_to_async(get_new_activity)(num=8)
-    testimonials = await sync_to_async(get_testimonial)()
-    achievements = await get_achievements(request)
+def home(request):
+    category = get_category()
+    new_activity = get_new_activity(num=8)
+    testimonials = get_testimonial()
+    achievements = get_achievements(request)
 
     if 'customer_id' not in request.session:
-        return await sync_to_async(render)(request, 'landing_page/index.html', {'category': category, 'new_activity': new_activity, 'testimonials': testimonials, 'achievements': achievements})
+        return render(request, 'landing_page/index.html', {'category': category, 'new_activity': new_activity, 'testimonials': testimonials, 'achievements': achievements})
     else:
         customer = request.session.get('customer_id')
         if customer[:2] == 'mi' :
-            data = await sync_to_async(get_mitra_data)(request)
+            data = get_mitra_data(request)
         elif customer[:2] == 'me':
-            data = await sync_to_async(get_member_data)(request)
-    
-    return await sync_to_async(render)(request, 'landing_page/index.html', {'data': data, 'category': category, 'new_activity': new_activity, 'testimonials': testimonials, 'achievements': achievements})
+            data = get_member_data(request)
+    print(achievements)
+    return render(request, 'landing_page/index.html', {'data': data, 'category': category, 'new_activity': new_activity, 'testimonials': testimonials, 'achievements': achievements})
 
 @cache_page(60*60*24)
 def about(request):
