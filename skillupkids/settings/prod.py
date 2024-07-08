afe3cd74ac217973949541c2ccfd821bf325ee74
eldivategar <tegareldiva@students.amikom.ac.id>
2024-07-08 21:21:06 +0700

    .

--- skillupkids/settings/prod.py
+++ skillupkids/settings/prod.py
@@ -2,7 +2,7 @@ import os
 from .settings import *
 
 SECRET_KEY = 'django-insecure-shst$+ksll6x&wmyrsy69%7r%(sd(o=9g2lw61d_16xkdv*n-)'
-DEBUG = True
+DEBUG = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
